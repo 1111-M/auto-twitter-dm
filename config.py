ef1cd2de248a99e3333ed3b63479be9610c13aac
1111-M <44734807+1111-M@users.noreply.github.com>
2018-11-10 02:14:11 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+	return '''Hey {}, YAAAAAS!'''.format(name)
 
 scheduler_time = 15 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "mchabots85" #change this to yours
